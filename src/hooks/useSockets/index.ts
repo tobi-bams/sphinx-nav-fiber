c568730a5ea8c23d08f4b1f2b46539685ab7b4b0
Oluwatobi Bamidele <teebams49@gmail.com>
2024-01-23 04:49:13 +0100

    fix: testing with ref

--- src/hooks/useSockets/index.ts
+++ src/hooks/useSockets/index.ts
@@ -1,21 +1,25 @@
 // useSocket.ts
 
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { io, Socket } from 'socket.io-client'
 import { API_URL } from '~/constants'
 
 const useSocket = (): Socket | null => {
   const [socket, setSocket] = useState<Socket | null>(null)
+  const isMounted = useRef<boolean>(true)
 
   useEffect(() => {
     console.log('We are trying to connect here')
 
     const socketInstance = io(API_URL)
 
-    setSocket(socketInstance)
+    if (isMounted.current) {
+      setSocket(socketInstance)
+    }
 
     return () => {
       console.log('I want to see what happens here')
+      isMounted.current = false
       socketInstance.disconnect()
     }
   }, [])
